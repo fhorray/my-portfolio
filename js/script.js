40df6bb89543cd39a998dc5a4d0c4c1aaaa36f4b
Francy Santos <francy.horray@gmail.com>
2023-01-11 19:58:07 -0300

    improvements and light-mode added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const body = document.querySelector("body");
 const languageBtn = document.querySelector("#language-btn");
 const languageContainer = document.querySelector(".lang-dropdown");
 // const headerContainer = document.querySelector("header");
@@ -5,3 +6,15 @@ const languageContainer = document.querySelector(".lang-dropdown");
 languageBtn.addEventListener("click", function () {
   languageContainer.classList.toggle("visible");
 });
+
+// light and dark mode
+const btnLightMode = document.querySelector("#light-mode");
+
+btnLightMode.addEventListener("click", function () {
+  body.classList.toggle("light-mode");
+});
+
+const heroContainer = document.querySelector(".s-hero .text");
+const h2El = document.querySelector(".s-hero .text h1").textContent;
+
+console.log(heroContainerH2.textContent);
