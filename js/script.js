d1c3aef2e2c89bcbb39ab8c877440cf607607f6b
Francy Santos <francy.horray@gmail.com>
2023-01-13 22:48:35 -0300

    links added

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ let imgMode = document.querySelector("#img-mode");
 
 modeBtn.addEventListener("click", function () {
   body.classList.toggle("light-mode");
-  if (imgMode.src === "http://127.0.0.1:5500/img/sun-i.svg") {
-    imgMode.src = "./img/moon-on-i.svg";
-  } else if (imgMode.src === "http://127.0.0.1:5500/img/moon-on-i.svg") {
-    imgMode.src = "./img/sun-i.svg";
+  if (imgMode.src === "https://fsantos.vercel.app//img/sun-i.svg") {
+    imgMode.src = "https://fsantos.vercel.app//img/moon-on-i.svg";
+  } else if (imgMode.src === "https://fsantos.vercel.app//img/moon-on-i.svg") {
+    imgMode.src = "https://fsantos.vercel.app//img/sun-i.svg";
   }
 });
 
