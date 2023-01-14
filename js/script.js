a4485a014cd53d6f339a283377b2a67811fd23c4
Francy Santos <francy.horray@gmail.com>
2023-01-13 22:53:18 -0300

    script dark mode fix

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ modeBtn.addEventListener("click", function () {
   body.classList.toggle("light-mode");
   if (imgMode.src === "https://fsantos.vercel.app/img/sun-i.svg") {
     imgMode.src = "https://fsantos.vercel.app/img/moon-on-i.svg";
-  } else if (imgMode.src === "https://fsantos.vercel.app//img/moon-on-i.svg") {
+  } else if (imgMode.src === "https://fsantos.vercel.app/img/moon-on-i.svg") {
     imgMode.src = "https://fsantos.vercel.app/img/sun-i.svg";
   }
 });
